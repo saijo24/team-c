002a98a6d7df1c45209820eed99b7324a07e897d
Rokusuke <2438takumi20000918@gmail.com>
2020-07-28 10:34:51 +0900

    Update response

---
+++ classification.py
@@ -0,0 +1,41 @@
+import utils
+from Classification_module.cl_basic import cl_basic
+from Classification_module.target import target_detect
+from Classification_module.cl_sub import cl_sub
+from pprint import pprint
+
+def classification(parsed_text):
+  classification_main_result =  cl_basic(parsed_text)
+  if len(classification_main_result) == 3:
+    status = classification_main_result.pop(2)
+  elif len(classification_main_result) == 2:
+    status = classification_main_result.pop(1)
+  else:
+    exit()
+  target = target_detect(parsed_text)
+  classification_sub_result = cl_sub(classification_main_result[0],parsed_text)
+  result = {
+    "basic_classification":classification_main_result,
+    "sub_classification":classification_sub_result,
+    "status":status,
+    "target":target,
+  }
+  return result
+
+def input_csv():
+  data = utils.open_csv(input('file_name>'))
+  for row in data:
+    print(row)
+    print(classification(utils.split_words(row)))
+    print("-------------------------")
+    input('next...')
+
+def main():
+  text = input('text>')
+  parsed_text = utils.split_words(text)
+  pprint(classification(parsed_text))
+
+if __name__ == "__main__":
+  while True:
+    #main()
+    input_csv()
--- utils.py
+++ utils.py
@@ -1,14 +1,23 @@
-
 import spacy
+import csv
 
 EXCEPTION_LIST  = ["ADP","AUX","PART","PUNCT", "NUM", "SYM"]
+
+def open_csv(file):
+  tmp = []
+  with open(file,encoding='shift-jis') as f:
+    reader = csv.reader(f)
+    data = [row for row in reader]
+  for i in range(len(data)): tmp.append(data[i][3])
+  return tmp
+  
 def split_words(text):
   nlp = spacy.load('ja_ginza')
   doc = nlp(text)
   exclusioned_text = []
   for sent in doc.sents:
     for token in sent:
-      if token.pos_ in EXCEPTION_LIST :#and token.is_stop == True or token.pos_ == "PUNCT":
+      if token.pos_ in EXCEPTION_LIST:
         if not str(token.orth_) == "？" or not str(token.lemma_) == "？":
           continue
       exclusioned_text.append(str(token.lemma_))
