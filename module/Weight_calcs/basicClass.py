ced7a3c69cdf4c1bbd988b008350c90e325b9bf8
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-16 07:55:17 +0900

    Update

--- module/Weight_calcs/basicClass.py
+++ module/Weight_calcs/basicClass.py
@@ -1,23 +1,5 @@
 from . import manageData as md
-
-# basicTag 基本分類辞書
-Tag = {
-  1:"Coming_out",
-  2:"Divined_inquested",
-  3:"Guard",
-  4:"Vote",
-  5:"Estimate",
-  6:"Agree",
-  7:"Disagree",
-  8:"Opinion",
-  9:"Information",
-  10:"Question",
-  11:"Ans",
-  12:"Request"
-}
-
-# swapedTag 辞書のキーと値を入れ替えたもの
-swapedTag = {val: key for key, val in Tag.items()}
+from . import datas as dts
 
 # Assem 重み計算用
 def Assem(data: dict, user: str):
