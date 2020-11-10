5a573e942519c155d3a68c1b3eb95cdf8a2092c0
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-10 09:21:50 +0900

    update

---
+++ module/weightcalctest.py
@@ -0,0 +1,26 @@
+from Weight_calc import basicClass as bc
+from Weight_calc import manageData as md
+
+# gotData テスト用
+gotData = {
+    'basic_classification': ['Coming_out', 'Request'],
+    'status': 'Disagree',
+    'sub_classification': 'Wolf',
+    'target': None
+}
+
+# users テスト用
+users = ("AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG")
+
+if __name__ == "__main__":
+    # 役職テーブルの初期化
+    md.initDatas(users)
+
+    # 分類結果辞書とユーザ名(or ID)を引数とする
+    # この関数を実行すれば重さ計算を終えて
+    # テーブルの更新も終了する
+    bc.assem(gotData, "AAA")
+    md.exiCo()
+
+    # 役職テーブルにあるデータを全て削除
+    md.fineDatas(users)
